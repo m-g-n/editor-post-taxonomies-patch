acef9cc1a8bae70530bb6f1d304e9041ccccbfc9
Hiroshi Urabe <mail@torounit.com>
2021-07-16 16:38:11 +0900

    Release 0.0.3

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Contributors:      Toro_Unit,megane9988
 Requires at least: 5.8  
 Tested up to:      5.8  
 Requires PHP:      7.3  
-Stable tag:        0.0.2
+Stable tag:        0.0.3
 License:           GPLv2 or later  
 License URI:       https://www.gnu.org/licenses/gpl-2.0.html
 
--- editor-post-taxonomies-patch.php
+++ editor-post-taxonomies-patch.php
@@ -3,7 +3,7 @@
  * Plugin Name: editor-post-taxonomies-patch
  * Plugin URI: https://github.com/m-g-n/editor-post-taxonomies-patch
  * Description: Upload Settings
- * Version: 0.0.2
+ * Version: 0.0.3
  * Author: m-g-n,HAMWORKS
  * Author URI: https://github.com/m-g-n/editor-post-taxonomies-patch
  * License: GPL2 or later
